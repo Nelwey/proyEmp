b0430ed9eac4e222b81ae1c6359e828a7883403d
Nelwey <nelwey1234@gmail.com>
2020-05-09 13:19:57 -0400

    configurando async login

--- server/routes/login.js
+++ server/routes/login.js
@@ -12,60 +12,40 @@ router.get('/', (req, res, next) => {
 
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
 
   let body = req.body;
-  Usuario.findOne({
-    ci: body.ci
-  }, (err, usuarioDB) => {
 
-    if (err) {
-      return res.status(500).json({
-        ok: false,
-        err
-      })
+  
+  try {
+    const usuarioDB = await Usuario.findOne({ ci: body.ci});
+    if(!usuarioDB){
+      throw new Error("(Usuario) o contraseña incorrectos");
     }
-
-    //si no existe el usuario
-    if (!usuarioDB) {
-      return res.status(400).json({
-        ok: false,
-        err: {
-          message: '(Usuario) o contraseña incorrectos'
-        }
-      });
-    }
-
-    //si la contraseña es erronea
     if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
-      // return res.status(400).json({
-      //   ok: false,
-      //   err: {
-      //     message: 'Usuario o (contraseña) incorrectos'
-      //   }
-      // });
-      res.status(400);
-      return res.send('Usuario o (contraseña) incorrectos');
-      
+
+      throw new Error("Usuario o (contraseña) incorrectos");      
     }
 
     let token = jwt.sign({
       usuario: usuarioDB
     }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
 
-    // res.status(200).json({
-    //   token,
-    //   usuarioDB
-    // });
-    res.status(200);
-    res.send(token);
-    // res.send(`Bienvenido al sistema ${usuarioDB.nombre}`);
-    
-  });
+    res.status(200).json({
+      token,
+      rol:usuarioDB.rol,
+      nombre:usuarioDB.nombre
+    });
+
+  } catch (error) {
+    res.status(404).json({
+      ok:false,
+      error:error.message
+    });
+  }
+
 });
-
-
 
 
 
